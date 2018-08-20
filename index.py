7d9a89fbf962ccc14a26ddda8306843c7fb0786f
BhawikaGupta <31989290+BhawikaGupta@users.noreply.github.com>
2018-08-20 22:22:10 +0530

    Adding Flask Solution

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, json
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+
+app = Flask(__name__)
+
+mysql = MySQL()
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'YAST'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+master_data = None
+
+@app.route("/")
+def main():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run()    
