433a377f1a387070fc81cdb4f2ca3d9fa19b0af2
BhawikaGupta <31989290+BhawikaGupta@users.noreply.github.com>
2018-08-22 22:55:41 +0530

    Adding signup functionality

--- index.py
+++ index.py
@@ -4,15 +4,31 @@ from werkzeug import generate_password_hash, check_password_hash
 import sys
 sys.path.append('config/')
 from dbConnect import dbConnect
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 db = dbConnect(app)
 
-@app.route("/signup")    
+@app.route("/signup",methods=['POST','GET'])    
 def signUp():
     if request.method == 'POST':
         conn, cursor = db.connect()
-        
+        userName = request.form['nameUser']
+        userEmail = request.form['emailUser']
+        userPassword = request.form['passwordUser']
+        sql = "select * from users where USERNAME='%s'" % (userEmail)
+        cursor.execute(sql)
+        rows = cursor.fetchone()
+        conn.commit()
+        if(rows):
+            return json.dumps({'message':'User Already Exists. Please login'}) 
+        else:
+            encrypted_password = sha256_crypt.encrypt(userPassword)
+            sql = "insert into users(USERNAME, NAME, PASSWORD) values('%s','%s','%s')" % (userEmail, userName, encrypted_password)
+            cursor.execute(sql)
+            conn.commit()
+            return json.dumps({'message':'success'}) 
+        db.disconnect(conn, cursor)
     else:
         return render_template('signup.html')    
 
@@ -28,9 +44,7 @@ def main():
         conn.commit()
         db.disconnect(conn, cursor)
         if(rows):
-            print rows[0]
-            print userPassword
-            if(rows[0] == userPassword):
+            if(sha256_crypt.verify(userPassword, rows[0])):
                 return json.dumps({'message':'success'}) 
             else:
                 return json.dumps({'message':'Enter correct password'})   
