bfb122e9bdb20c9beb312102aae5b281913983e4
BhawikaGupta <31989290+BhawikaGupta@users.noreply.github.com>
2018-08-31 22:57:55 +0530

    Moving Django Solution

---
+++ login/models.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import connection
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from random import randint
+
+class users_profile(models.Model):
+    USEREMAIL = models.CharField(max_length = 100)
+    CONTACT = models.IntegerField()
+    BLOOD_GP = models.CharField(max_length = 100)
+    GENDER = models.CharField(max_length = 100)
+    AGE = models.IntegerField()
+    LOCATION = models.CharField(max_length = 100)
+    DONATE_Bf = models.CharField(max_length = 100)
+    class Meta:
+      db_table = "users_profile"
+
+class users(models.Model):
+    USERNAME = models.CharField(max_length = 100)
+    NAME = models.CharField(max_length = 100)
+    PASSWORD = models.CharField(max_length = 100)
+    DEPARTMENT = models.CharField(max_length = 100)
+    class Meta:
+      db_table = "users"
+    
+class userOTP(models.Model):
+    OTP_PURPOSE_CHOICES = (('FP', 'Forgot Password'),('AA', 'Activate Account'));
+    user = models.ForeignKey(users)
+    otp = models.CharField(max_length = 4)
+    purpose = models.CharField(max_length = 2, choices = OTP_PURPOSE_CHOICES)
+    created_on = models.DateTimeField(auto_now_add = True)
+    class Meta:
+        db_table = "userOTP"
+        unique_together= ['user', 'purpose']
+
+def create_otp(user = None, purpose = None):
+    choices = []
+    for choice_purpose, verbose in userOTP.OTP_PURPOSE_CHOICES:
+        choices.append(choice_purpose)
+    if not purpose in choices:
+        raise ValueError('Invalid Arguments');
+    if userOTP.objects.filter(user = user, purpose = purpose).exists():
+        old_otp = userOTP.objects.get(user = user, purpose = purpose)
+        old_otp.delete()
+    otp = randint(1000, 9999)
+    otp_object = userOTP.objects.create(user = user, purpose = purpose, otp = otp)
+    return otp
+
+def get_valid_otp_object(user = None, otp= None, purpose = None):
+    if not user:
+        raise ValueError("Invalid Arguments");
+    choices = []
+    for choice_purpose, verbose in userOTP.OTP_PURPOSE_CHOICES:
+        choices.append(choice_purpose)
+    if not purpose in choices:
+        raise ValueError('Invalid Arguments');
+    try:
+        otp_object = userOTP.objects.get(user = user, purpose=purpose, otp=otp)
+        return otp_object
+    except userOTP.DoesNotExist:
+        return None
+    
+def verifyLogin(userEmail):
+    login_object = users.objects.get(USERNAME=userEmail)
+    return login_object
+
+def checkUser(userName):
+    login_object = users.objects.get(USERNAME=userName)
+    return login_object
+    
+def signUpUser(userEmail, userName, encrypted_password, dept):
+    users = users(USERNAME=userEmail, NAME=userName, PASSWORD=encrypted_password, DEPARTMENT=dept)
+    cursor.close()
+    
+def GetUserDetails(userEmail):
+    user_object = users_profile.objects.get(USEREMAIL=userEmail)
+    return user_object
---
+++ login/views.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http import HttpResponse
+from models import *
+from passlib.hash import sha256_crypt
+import json
+from django.template import RequestContext
+from collections import OrderedDict
+from django.core.mail import send_mail
+from datetime import datetime
+from django.core.mail import EmailMessage
+
+def homePage(request):
+    return render(request, 'homePage.html', {})
+
+def reset_password(request,id = None, otp = None):
+    if request.method == 'GET':
+        return render(request, 'resetPassword.html', {});
+    elif request.method == 'POST':
+        f = RefillForm(request.POST)
+        if not f.is_valid():
+         context = { 'f' : f, 'otp': otp_object.otp, 'uid': user.id}
+         return render(request, 'accounts/auth/refill.html', context)
+        else:
+         user.set_password(f.cleaned_data['new_password'])
+         user.save()
+         otp_object.delete()
+         return render(request, 'accounts/auth/set_password_success.html', { 'u' : user,'shop_list' : Shops.objects.all()})
+
+def resetUser(request):
+    if request.method == 'GET':
+        return render(request, 'resetUser.html', {})
+    elif request.method == 'POST':
+        userEmail = request.POST['userEmailConfirm']
+        rows = checkUser(userEmail)
+        if(rows):
+            otp = create_otp(user = userEmail, purpose = 'FP')
+            link='http://127.0.0.1:8000/accounts/reset/%s/%s' % (userEmail,otp)
+            print link
+            email = EmailMessage('Subject', 'Body', to=[userEmail])
+            email.send()
+            #return render(request, 'accounts/auth/reset.html', {'u': user,'shop_list' : Shops.objects.all()})
+            return HttpResponse(str(json.dumps({'message':'Password reset Email has been sent to your link. Please reset and login!'})))
+        else:
+            return HttpResponse(str(json.dumps({'message':'Entered username is not registered! Please Signup!'})))
+        
+
+def cuDashboard(request):
+    login_info = request.session.get('username', "guest")
+    print login_info[1]
+    user = OrderedDict() 
+    user["useremail"]=login_info[0],
+    user["username"]=login_info[1].encode("utf-8"),
+    user["contact"]="",
+    user["blood"]="",
+    user["gender"]="",
+    user["age"]="",
+    user["location"]="",
+    user["donate"]=""
+    rows = GetUserDetails(login_info[0])
+    if(rows):
+        count = 0 
+        for key in user:
+            if key == "username":
+                continue
+            else:
+                user[key] = rows[count].encode("utf-8")
+                count = count +1
+    user["username"]=user["username"][0]
+    #user_data = dict(user)
+    #print user_data
+    #print type(user_data)
+    #return HttpResponse('cuDashboard.html',user_data) 
+    print user
+    return render(request, 'cuDashboard.html', {"data":user})
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, "login.html", {})
+    elif request.method == 'POST':
+        userEmail = request.POST['userEmail']
+        userPassword = request.POST['userPassword']
+        rows = verifyLogin(userEmail)
+        if(rows):
+            if(sha256_crypt.verify(userPassword, rows[2])):
+                request.session['username'] = [rows[0],rows[1]]
+                return  HttpResponse(str(json.dumps({'message':'success'}) ))
+            else:
+                return HttpResponse(str(json.dumps({'message':'Enter correct password'})))
+        else:
+            return HttpResponse(str(json.dumps({'message':'Entered username is not registered'})))
+   
+   
+def signup(request):
+    if request.method == 'GET':
+        return render(request, "signup.html", {})
+    elif request.method == 'POST':
+        userName = request.POST['nameUser']
+        userEmail = request.POST['emailUser']
+        userPassword = request.POST['passwordUser']
+        dept = "Volunteer"
+        row = checkUser(userEmail)
+        if(row):
+            return HttpResponse(str(json.dumps({'message':'User Already Exists. Please login'})))
+        else:
+            encrypted_password = sha256_crypt.encrypt(userPassword)
+            signUpUser(userEmail, userName, encrypted_password, dept)
+            return HttpResponse(str(json.dumps({'message':'success'})))
+    
