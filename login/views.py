895942af893af7c925dcb8ed11d85d19e39a0aa6
BhawikaGupta <31989290+BhawikaGupta@users.noreply.github.com>
2018-09-01 18:39:12 +0530

    Validating password Request

--- login/views.py
+++ login/views.py
@@ -9,27 +9,38 @@ from django.template import RequestContext
 from collections import OrderedDict
 from django.core.mail import send_mail
 from datetime import datetime
-#from django.core.mail import EmailMessage
+from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.urls import resolve
+   
 
 def homePage(request):
     return render(request, 'homePage.html', {})
 
-def reset_password(request,id = None, otp = None):
+def reset_password(request, id, otp):
+    user = {}
     if request.method == 'GET':
-        return render(request, 'resetPassword.html', {});
-    elif request.method == 'POST':
-        f = RefillForm(request.POST)
-        if not f.is_valid():
-         context = { 'f' : f, 'otp': otp_object.otp, 'uid': user.id}
-         return render(request, 'accounts/auth/refill.html', context)
+        rows, time_model = get_valid_otp_object(id, otp)
+        if(rows):
+            return render(request, 'resetPassword.html', {})
         else:
-         user.set_password(f.cleaned_data['new_password'])
-         user.save()
-         otp_object.delete()
-         return render(request, 'accounts/auth/set_password_success.html', { 'u' : user,'shop_list' : Shops.objects.all()})
-
+            user["message"] = "You entered an Invalid Link"
+            return render(request, 'errorPage.html', {"data":user})
+    elif request.method == 'POST':
+        rows, time_model = get_valid_otp_object(id, otp)
+        if(rows):
+            resetPassword = request.POST['resetPassword']
+            encrypted_password = sha256_crypt.encrypt(resetPassword)
+            if(sha256_crypt.verify(resetPassword, time_model.PASSWORD)):
+                return HttpResponse(str(json.dumps({'message':'Old Password and New Password are same. Try another password'})))
+            else:
+                set_password_otp(time_model, encrypted_password)
+                return HttpResponse(str(json.dumps({'message':'Password reset successfully! Please Login!'})))
+        else:
+            user["message"] = "You entered an Invalid Link"
+            return render(request, 'errorPage.html', {"data":user})
+            
 def resetUser(request):
     if request.method == 'GET':
         return render(request, 'resetUser.html', {})
@@ -38,7 +49,10 @@ def resetUser(request):
         rows = checkUser(userEmail)
         if(rows):
             id, otp, userName = create_otp(user = userEmail, purpose = 'FP')
-            link='http://127.0.0.1:8000/accounts/reset/%s/%s/' % (id,otp)
+            HOST_METHOD = getattr(settings, "HOST_METHOD", None)
+            HOST_ADDRESS = getattr(settings, "HOST_ADDRESS", None)
+            HOST_PORT = getattr(settings, "HOST_PORT", None)
+            link = HOST_METHOD + '://' + HOST_ADDRESS + ':' + HOST_PORT + '/accounts/reset/%s/%s/' % (id,otp)
             msg_html = render_to_string('passwordRestMailTemplate.html', { 'username': userName, 'link':link})
             send_mail('Password Reset Request',"Hi",None,[userEmail],html_message=msg_html)
             return HttpResponse(str(json.dumps({'message':'Password reset Email has been sent to your link. Please reset and login!'})))
